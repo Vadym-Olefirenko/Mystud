c244f32f2a3a95d5fe9201ff3e3d3b3a5a541555
Vadym <laimg011@gmail.com>
2020-02-12 22:03:10 +0200

    first commit

---
+++ assets/main.js
@@ -0,0 +1,28 @@
+// 'use strict';
+
+// let money = prompt('Ваш бюджет на месяц', ''),
+//     time = prompt('Введите дату в формате YYYY-MM-DD', ''),
+//     mustExpence = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//     howMuch = prompt('Во сколько обойдется?', '');
+
+
+// let appData = {
+//     moneyData: money,
+//     timeData: time,
+//     expences: {
+       
+//     },
+//     optionalExpences: {
+
+//     },
+//     income: [
+
+//     ],
+//     savings: false,
+// };
+
+// for( let i = 0; i < 2; i+) {
+
+// };
+
+// alert(appData.moneyData / 30);
