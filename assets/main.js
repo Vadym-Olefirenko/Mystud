e3ed13a8f846d35328ee27d11c7182cfd5b53f8b
Vadym <laimg011@gmail.com>
2020-02-12 22:08:38 +0200

    second commit

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,30 @@
-// 'use strict';
+'use strict';
 
-// let money = prompt('Ваш бюджет на месяц', ''),
-//     time = prompt('Введите дату в формате YYYY-MM-DD', ''),
-//     mustExpence = prompt('Введите обязательную статью расходов в этом месяце', ''),
-//     howMuch = prompt('Во сколько обойдется?', '');
+let money = +prompt('Ваш бюджет на месяц', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    
 
 
-// let appData = {
-//     moneyData: money,
-//     timeData: time,
-//     expences: {
+let appData = {
+    moneyData: money,
+    timeData: time,
+    expences: {
        
-//     },
-//     optionalExpences: {
+    },
+    optionalExpences: {
 
-//     },
-//     income: [
+    },
+    income: [
 
-//     ],
-//     savings: false,
-// };
+    ],
+    savings: false,
+};
 
-// for( let i = 0; i < 2; i+) {
+for( let i = 0; i < 2; i++) {
+    let = mustExpence = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        howMuch = +prompt('Во сколько обойдется?', '');
 
-// };
+    appData.expences[mustExpence] = howMuch;
+};
 
-// alert(appData.moneyData / 30);
+alert(appData.moneyData / 30);
